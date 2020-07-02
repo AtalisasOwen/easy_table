57de4349111644517c32006f63c9c6cdf7c5ceb9
86189 <742137319@qq.com>
2020-07-02 21:12:42 +0800

    增加插槽能力

--- easy_table/src/App.vue
+++ easy_table/src/App.vue
@@ -2,8 +2,9 @@
     <div id="app">
         <Hello/>
         <EasyTable :easy-columns="columns"
-                :data="form"
-                :form-methods="formMethods">
+                   :data="data"
+                   :table-methods="tableMethods"
+                   :form-methods="formMethods">
             <template #dprt.name="props">
                 <el-tag>{{ props.row.dprt.name }}</el-tag>
             </template>
@@ -12,133 +13,139 @@
 </template>
 
 <script>
-    import Hello from "./components/Hello.jsx";
-    import EasyForm from "./components/EasyForm";
-    import EasyTable from "./components/EasyTable";
+    import Hello from './components/Hello.jsx'
+    import EasyTable from './components/EasyTable'
 
     export default {
-        name: "App",
+        name: 'App',
         data() {
             return {
-                form: [
+                count: 0,
+                data: [
                     {
-                        date: "2016-05-02",
-                        name: "王小虎1",
-                        address: "上海市普陀区金沙江路 1518 弄",
-                        money: 20006600,
+                        date: '2016-05-02',
+                        name: '王小虎1',
+                        address: '上海市普陀区金沙江路 1518 弄',
+                        money: 6600,
                         dprt: {
                             id: 1,
-                            name: "A部门",
-                            phone: "021-0000000",
+                            name: 'A部门',
+                            phone: '021-0000000'
                         },
                         ddd: {
                             dsdsd: {
-                                birthday: "2019-01-02T08:00:00",
-                            },
-                        },
+                                birthday: '2019-01-02T08:00:00'
+                            }
+                        }
                     },
                     {
-                        date: "2016-05-02",
-                        name: "王小虎2",
-                        address: "上海市普陀区金沙江路 1518 弄",
-                        money: 20006600,
+                        date: '2016-05-02',
+                        name: '王小虎2',
+                        address: '上海市普陀区金沙江路 1518 弄',
+                        money: 26600,
                         dprt: {
                             id: 1,
-                            name: "A部门",
-                            phone: "021-0000000",
+                            name: 'A部门',
+                            phone: '021-0000000'
                         },
                         ddd: {
                             dsdsd: {
-                                birthday: "2019-01-02T08:00:00",
-                            },
-                        },
+                                birthday: '2019-01-02T08:00:00'
+                            }
+                        }
                     },
                     {
-                        date: "2016-05-02",
-                        name: "王小虎3",
-                        address: "上海市普陀区金沙江路 1518 弄",
+                        date: '2016-05-02',
+                        name: '王小虎3',
+                        address: '上海市普陀区金沙江路 1518 弄',
                         money: 20006600,
                         dprt: {
                             id: 1,
-                            name: "A部门",
-                            phone: "021-0000000",
+                            name: 'A部门',
+                            phone: '021-0000000'
                         },
                         ddd: {
                             dsdsd: {
-                                birthday: "2019-01-02T08:00:00",
-                            },
-                        },
-                    },
+                                birthday: '2019-01-02T08:00:00'
+                            }
+                        }
+                    }
                 ],
-                emptyForm: {},
                 columns: [
                     {
-                        prop: "date",
-                        label: "日期",
-                        valueType: "date",
+                        prop: 'date',
+                        label: '日期',
+                        valueType: 'date'
                     },
                     {
-                        prop: "money",
-                        label: "工资",
-                        valueType: "money",
+                        prop: 'money',
+                        label: '工资',
+                        valueType: 'money',
+                        sortable: true
                     },
                     {
-                        prop: "name",
-                        label: "姓名",
+                        prop: 'name',
+                        label: '姓名',
+                        sortable: true
                     },
                     {
-                        prop: "address",
-                        label: "地址",
+                        prop: 'address',
+                        label: '地址'
                     },
                     {
-                        prop: "dprt.id",
-                        label: "部门ID",
-                        valueType: "enum",
+                        prop: 'dprt.id',
+                        label: '部门ID',
+                        valueType: 'enum',
                         width: 100,
-                        enumUrl: "/api/XXX/",
+                        enumUrl: '/api/XXX/',
                         enumItems: [
                             {
-                                value: "A部门", id: 1,
+                                value: 'A部门', id: 1
                             },
                             {
-                                value: "B部门", id: 2,
-                            },
+                                value: 'B部门', id: 2
+                            }
                         ],
                         hideInTable: true
                     },
                     {
-                        prop: "dprt.name",
-                        label: "部门名称",
+                        prop: 'dprt.name',
+                        label: '部门名称',
                         hideInForm: true,
                         render: Hello
                     },
                     {
-                        prop: "dprt.phone",
-                        label: "部门电话",
+                        prop: 'dprt.phone',
+                        label: '部门电话'
                     },
                     {
-                        prop: "ddd.dsdsd.birthday",
-                        label: "生日",
-                        valueType: "datetime",
-                        width: 180,
-                    },
+                        prop: 'ddd.dsdsd.birthday',
+                        label: '生日',
+                        valueType: 'datetime',
+                        width: 180
+                    }
                 ],
-                formMethods:{
+                formMethods: {
                     onSubmitCreate: (form) => {
-                        console.log("cccc")
-                        console.log(JSON.stringify(form));
+                        console.log('cccc')
+                        console.log(JSON.stringify(form))
                     },
                     onSubmitUpdate: (form) => {
-                        console.log("uuuu")
-                        console.log(JSON.stringify(form));
+                        console.log('uuuu')
+                        console.log(JSON.stringify(form))
+                    }
+                },
+                tableMethods: {
+                    sortChange: ({ prop, order }) => {
                     }
                 }
-            };
+            }
         },
+        methods: {},
         components: {
-            Hello, EasyForm, EasyTable,
-        },
-    };
+            Hello, EasyTable
+        }
+    }
 </script>
 
 <style lang="less">
--- easy_table/src/components/EasyTable.jsx
+++ easy_table/src/components/EasyTable.jsx
@@ -1,13 +1,14 @@
-import EasyForm from "./EasyForm";
-
+import EasyForm from './EasyForm'
+import CommonSearch from './CommonSearch'
 
 export default {
     props: {
-      data: Array,
-      easyColumns: Array,
-      formMethods: Object
+        data: Array,
+        easyColumns: Array,
+        formMethods: Object,
+        tableMethods: Object
     },
-    data(){
+    data() {
         return {
             form: null,
             dialogVisible: false
@@ -18,65 +19,72 @@ export default {
             this.$confirm('确认关闭？')
                 .then(_ => {
                     this.dialogVisible = false
-                    done();
+                    done()
                 })
-                .catch(_ => {});
+                .catch(_ => {
+                })
         },
-      onEdit(props){
-          return () =>{
-              this.form = props.row;
-              this.dialogVisible = true;
-          }
-      },
-        onCreate(){
+        onEdit(props) {
+            return () => {
+                this.form = props.row
+                this.dialogVisible = true
+            }
+        },
+        onCreate() {
             this.form = undefined
-            this.dialogVisible = true;
+            this.dialogVisible = true
         }
     },
-    render(h){
+    render(h) {
         const elColumns = this.easyColumns
             .filter(c => !c.hideInTable)
             .map(c =>
-            <el-table-column
-            prop={c.prop}
-            label={c.label}
-            show-overflow-tooltip={true}
-            width={c.width ? c.width : 120}
-            scopedSlots={{
-                default: this.$scopedSlots[c.prop]
-            }}
-            >
-            </el-table-column>);
+                <el-table-column
+                    prop={ c.prop }
+                    label={ c.label }
+                    show-overflow-tooltip={ true }
+                    width={ c.width ? c.width : 120 }
+                    sortable={ c.sortable ? 'custom' : false }
+                    scopedSlots={ {
+                        default: this.$scopedSlots[c.prop]
+                    } }
+                >
+                </el-table-column>)
         return (
             <div>
-                <div>
-                    <el-button icon="el-icon-plus" type="primary" onClick={this.onCreate}>新建</el-button>
-                </div>
+                <CommonSearch
+                    columnOptions={this.easyColumns}
+                    scopedSlots={{
+                        right: (props) => <span style="float: right;margin-right: 10%;"><el-button icon="el-icon-plus" type="primary" onClick={ this.onCreate }>新建</el-button></span>
+                    }}
+                >
                 <el-table
-                    data={this.data}
-                    style="width: 100%">
-                    {...elColumns}
+                    data={ this.data }
+                    style="width: 100%"
+                    vOn:sort-change={ this.tableMethods.sortChange }
+                >
+                    { ...elColumns }
                     <el-table-column
                         fixed="right"
                         label="操作"
-                        width="180" scopedSlots={{
+                        width="180" scopedSlots={ {
                         default: props => {
                             return (
                                 <span>
-                                    <el-button onClick={this.onEdit(props)} type="text" size="small">编辑</el-button>
+                                    <el-button onClick={ this.onEdit(props) } type="text" size="small">编辑</el-button>
                                     <el-button type="text" size="small">删除</el-button>
                                 </span>
                             )
                         }
-                    }}>
+                    } }>
                     </el-table-column>
                 </el-table>
                 <el-dialog
-                    before-close={this.handleClose}
+                    before-close={ this.handleClose }
                     title="提示"
-                    visible={this.dialogVisible}
+                    visible={ this.dialogVisible }
                     width="35%">
-                    <EasyForm easyForm={ this.form } easyColumns={this.easyColumns} methods={
+                    <EasyForm easyForm={ this.form } easyColumns={ this.easyColumns } methods={
                         {
                             onCancel: () => {
                                 this.dialogVisible = false
@@ -88,10 +96,11 @@ export default {
                             onSubmitUpdate: (form) => {
                                 this.formMethods.onSubmitUpdate(form)
                                 this.dialogVisible = false
-                            },
+                            }
                         }
-                    } />
+                    }/>
                 </el-dialog>
+                </CommonSearch>
             </div>
 
         )
